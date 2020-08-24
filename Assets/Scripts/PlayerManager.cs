5ca4491e03ff2ed500c7180842f81d9da882c168
VidarVigg <vidar.martensson@outlook.com>
2020-08-24 14:37:25 +0200

    Started working on a player Prototype

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody playerRigidbody;
+
+    private Vector3 movementVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void AttachInput()
+    {
+        InputManager.INSTANCE.onMoveKeyPressed += Move;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRigidbody.velocity = movementVector;
+    }
+
+    private void Move(Vector3 input)
+    {
+        
+    }
+}
